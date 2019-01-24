e4c4e2d2116f48a2960bfa586dcd58004ce1415e
OWID <owid@ourworldindata.org>
2019-01-24 22:58:50 +0000

    Code deployment update

---
+++ grapher/embedCharts.js
@@ -0,0 +1,38 @@
+
+        window.App = {};
+
+        var link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = 'https://mispytest-owid.netlify.com/assets/css/commons.css';
+        document.head.appendChild(link);
+
+        var hasPolyfill = false;
+        var hasGrapher = false;
+
+        var loadedScripts = 0;
+        function checkReady() {
+            loadedScripts += 1;
+            if (loadedScripts == 3) {
+                window.Grapher.embedAll();
+            }
+        }
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = checkReady;
+        script.src = "https://cdn.polyfill.io/v2/polyfill.min.js?features=es6,fetch"
+        document.head.appendChild(script);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = checkReady;
+        script.src = 'https://mispytest-owid.netlify.com/assets/js/commons.js';
+        document.head.appendChild(script);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = checkReady;
+        script.src = 'https://mispytest-owid.netlify.com/assets/js/owid.js';
+        document.head.appendChild(script);
+    
